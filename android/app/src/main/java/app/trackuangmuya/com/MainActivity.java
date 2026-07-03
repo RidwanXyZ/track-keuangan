b0c422dced26fc1f77e1793c9d4f553170a9ca1f
RidwanXyZ <ridwanwiki18220@gmail.com>
2026-07-03 22:23:57 +0700

    Initial React + Capacitor

---
+++ android/app/src/main/java/app/trackuangmuya/com/MainActivity.java
@@ -0,0 +1,5 @@
+package app.trackuangmuya.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.trackuangmuya.com',
+  appName: 'track-keuangan',
+  webDir: 'dist'
+};
+
+export default config;
